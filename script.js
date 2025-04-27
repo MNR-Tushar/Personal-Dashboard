feacf5ee061c6970690df40ae3f68b68c7eebe63
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-04-27 16:19:15 +0600

    Weather fetch and catch function done

--- script.js
+++ script.js
@@ -9,4 +9,15 @@ function updateclock()
     const now= new Date();
     timeEl.textContent=now.toLocaleTimeString();
 }
-setInterval(updateclock)
+setInterval(updateclock);
+
+fetch("http://api.weatherapi.com/v1/current.json?key=f7b41d9fdf4c436aa7a192633252504&q=Bangladesh")
+.then((res)=>res.json())
+.then((data)=>{
+    const w=document.getElementById("setweather");
+    w.textContent=`${data.location.name}: ${data.current.temp_c},${data.current.condition.text}`
+})
+.catch((err)=>{
+    const w=document.getElementById("setweather");
+    w.textContent="Please given proper country name or loacation";
+});
