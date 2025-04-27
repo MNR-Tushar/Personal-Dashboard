90f843079ccea24618fa885abf7f31228bc1949d
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-04-27 16:05:08 +0600

    updateclock function is done

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleTheme()
 {
     document.body.classList.toggle("dark");
 }
+
+function updateclock()
+{
+    const timeEl=document.getElementById("clock");
+    const now= new Date();
+    timeEl.textContent=now.toLocaleTimeString();
+}
+setInterval(updateclock)
